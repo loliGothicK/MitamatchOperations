c83a0715fb77b8b991683b0908cf259f9bd8ece8
mitama <mitama@mitama.tech>
2022-10-15 22:54:20 +0900

    コミットするの忘れてたわ

---
+++ MitamatchOperations/MitamatchOperations/Do.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ABI.System.Windows.Input;
+using mitama.Pages.OrderConsole;
+using static System.Collections.Specialized.BitVector32;
+
+namespace mitama;
+
+internal record Defer(Action Action) : IDisposable, ICommand
+{
+    void IDisposable.Dispose() => Action();
+
+    public event EventHandler? CanExecuteChanged;
+
+    bool System.Windows.Input.ICommand.CanExecute(object? _) => true;
+
+    void System.Windows.Input.ICommand.Execute(object? _) => Action.Invoke();
+}
