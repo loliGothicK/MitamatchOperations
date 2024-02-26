0ce5257d31e431a3959307119a452d484f418f3d
mitama <loligothick@gmail.com>
2024-02-26 20:27:52 +0900

    chore: costom scheme

--- MitamatchOperations/App.xaml.cs
+++ MitamatchOperations/App.xaml.cs
@@ -1,6 +1,5 @@
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using mitama;
-using Mitama.Pages.Common;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
